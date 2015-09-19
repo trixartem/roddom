5e4bd85415702cd493ce3cfd88f46ba436cc271c
Artem Kuvaldin <akuv@yandex-team.ru>
2015-09-19 18:52:08 +0500

    final about

--- about-2/JS/bundle.js
+++ about-2/JS/bundle.js
@@ -1,3 +1,28 @@
-var s = require('./header');
-console.log('asd');
-s();
+require('./header');
+require('./gmaps')();
+require('./form')();
+var iScroll = require('./iscroll');
+
+function scroll() {
+    var myScroll = new iScroll('#gallery', {
+        freeScroll: true,
+        mouseWheel: true,
+        scrollbars: true,
+        bounce: false,
+        scrollX: true
+    });
+}
+document.addEventListener('DOMContentLoaded', scroll, false);
+var items = [];
+$('.carousel-item').each(function (key, item) {
+    var $item = $(item);
+    var target = Number($item.data('target'))
+    if (target || target === 0) {
+        items[target] = key;
+    }
+})
+var carousel = $('#carousel');
+$('.gallery__item').click(function () {
+    var id = $(this).data('id');
+    carousel.carousel(items[id])
+})
--- about-2/JS/header.js
+++ about-2/JS/header.js
@@ -1,6 +1,6 @@
 module.exports = function (s) {
     console.log('bbb', s);
-    console.log($('body.qqqqq'));
+    console.log($('body'));
     return;
 }
 
